51f02efdc12ed1b7c241f0003c421a8002860c96
Eithar <eitharyahia@yahoo.com>
2020-03-06 08:22:43 +0200

    Updates

---
+++ precache-manifest.755f72c6ea069e22c3d35242119b90ba.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3922895428d55a5ae844754cb10436b",
+    "url": "/toDoList/index.html"
+  },
+  {
+    "revision": "ed4ccbcca3229f80a82c",
+    "url": "/toDoList/static/js/2.c128a862.chunk.js"
+  },
+  {
+    "revision": "c81a2cd2fb1d8371109f",
+    "url": "/toDoList/static/js/main.60e015d1.chunk.js"
+  },
+  {
+    "revision": "05c7a624ab4622bd1380",
+    "url": "/toDoList/static/js/runtime-main.5bbc155b.js"
+  }
+]);
